f723eef1b61b8e5b085778e7a47d63658143d36f
Mohamed Atta <mohamedatta24@gmail.com>
2026-04-13 08:24:40 +0200

    finsh app colors and app text styles

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFF000046);
+  static const Color primaryColor = Color(0xFF000046);
+  static const Color secondaryColor = Color(0xFFFF1DDC);
+  static const Color textColor = Color(0xFFF3F2EC);
+  static const Color grayColor = Color(0xFFB0B0B0);
+}
---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyles {
+  static const String fontFamily = 'Roboto';
+
+  static const TextStyle bold22 = TextStyle(
+    fontFamily: fontFamily,
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+  );
+  static const TextStyle medium16 = TextStyle(
+    fontFamily: fontFamily,
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+  );
+}
